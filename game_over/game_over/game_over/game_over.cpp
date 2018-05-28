ea90302d655fd69fde43c4fc16f7dbbec62adfd2
pdsiedler <p.d.siedler@gmail.com>
2018-05-28 22:53:29 +0100

    initializing repo

---
+++ game_over/game_over/game_over/game_over.cpp
@@ -0,0 +1,14 @@
+//Game Over
+//A first C++ program
+
+//#using namespace std;
+
+#include <iostream>
+using std::cout;
+using std::endl;
+
+int main()
+{
+	cout << "Game Over" << endl;
+	return 0;
+}
